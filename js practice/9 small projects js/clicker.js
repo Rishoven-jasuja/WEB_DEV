cb336b0b8d550ddda21528b28ea3f641783b7fe8
unknown <jasujarishu1@gmail.com>
2026-01-19 11:28:48 +0530

    clicker complete

--- js practice/9 small projects js/clicker.js
+++ js practice/9 small projects js/clicker.js
@@ -1,4 +1,23 @@
 console.log("hello");
-window.addEventListener('click', (e) => {
+const body=document.querySelector('body');
+window.addEventListener('mousemove', (e) => {
     console.log(e.clientX, e.clientY);
+
+    const circ=document.createElement('div');
+circ.classList.add('circle');
+
+const color=['red','blue','pink','white','green','yellow', 'orange','purple']
+circ.style.backgroundColor=color[Math.floor(Math.random()*color.length)]
+circ.style.top=`${e.clientY}px`;
+circ.style.left=`${e.clientX}px`;
+body.append(circ);
+
+circ.textContent="hii";
+setTimeout(()=>{
+    circ.remove();
+},4000)
 });
+
+
+
+
